e0c643e1e21a52f378a05fc68b10466cdcb971ae
Jasbir Singh <jasbir212singh@gmail.com>
2023-06-09 14:50:48 +0530

    added state for handling register form

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,18 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleInputChanges = (type, e) => {
+    const {value} = e?.target;
+
+    switch (type) {
+      case "name":
+        setName(value);
+        break;
+      case "email":
+        setEmail(value);
+        break;
+      case "password":
+        setPassword(value);
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    
+
+  }
+
   return (
     <div className="flex flex-col items-center justify-center mt-12">
       <div className="border border-gray-300 p-6 w-2/6 rounded-xl shadow-lg">
         <h1 className="text-4xl mb-6 text-center text-primary">Register</h1>
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={handleFormSubmit}>
           <div className="flex flex-col mb-5">
             <label className="mb-1">Enter Your Name :</label>
             <input
               className="border border-gray-300 p-2 rounded-full focus:border-primary focus:outline-none focus:shadow-input"
               type="text"
+              value={name}
               placeholder="eg: Johnson"
+              onChange={(e) => handleInputChanges("name", e)}
             />
           </div>
           <div className="flex flex-col mb-5">
@@ -20,7 +51,9 @@ const Register = () => {
             <input
               className="border border-gray-300 p-2 rounded-full focus:outline-none focus:shadow-input focus:border-primary"
               type="email"
+              value={email}
               placeholder="eg: youremail@gmail.com"
+              onChange={(e) => handleInputChanges("email", e)}
             />
           </div>
           <div className="flex flex-col mb-5">
@@ -28,7 +61,9 @@ const Register = () => {
             <input
               className="border border-gray-300 p-2 rounded-full focus:outline-none focus:shadow-input focus:border-primary"
               type="password"
+              value={password}
               placeholder="eg: yourname@123#"
+              onChange={(e) => handleInputChanges("password", e)}
             />
           </div>
           <button className="bg-primary text-white p-3 rounded-full w-full mt-1">
