184977a176f9cc73f7620015afda98c2b748c6ea
Jasbir Singh <jasbir212singh@gmail.com>
2023-06-09 12:00:47 +0530

    utilized Outlet(react-router-dom, v6) for creating Layout for pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,17 @@ import Header from "./components/Header";
 import Index from "./pages/Index";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Layout from "./components/Layout";
 
 const App = () => {
   return (
     <div className="App">
-      <Header />
       <Routes>
-        <Route index element={<Index />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />}/>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Index />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
       </Routes>
     </div>
   );
