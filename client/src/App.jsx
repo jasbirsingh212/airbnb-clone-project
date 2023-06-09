0e2e9a6751dd8c65dbe7145b7ec45a19053aedc9
Jasbir Singh <jasbir212singh@gmail.com>
2023-06-09 11:30:16 +0530

    created index login register pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
+import Index from "./pages/Index";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route index element={<Index />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />}/>
+      </Routes>
     </div>
   );
 };
