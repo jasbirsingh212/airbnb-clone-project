6870b3b57c656fe8ec2e4b97f3dcbc8e8115d963
Jasbir Singh <jasbir212singh@gmail.com>
2023-06-09 15:27:58 +0530

    created api to register user

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT ||  4000
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Server listening on port ${port}!`))
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://127.0.0.1:5173",
+  })
+);
+const port = process.env.PORT || 4000;
+
+app.get("/register", (req, res) => res.send("Hello World! " + req.name));
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
