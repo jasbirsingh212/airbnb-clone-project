483ad53f87548e4f40e4f5bc487a744d91083583
Jasbir Singh <jasbir212singh@gmail.com>
2023-06-09 16:51:29 +0530

    connected to mogodb with mongoose

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
+require("dotenv").config();
 
 app.use(express.json());
 
@@ -10,7 +12,21 @@ app.use(
     origin: "http://127.0.0.1:5173",
   })
 );
+
+try {
+  mongoose.connect(`${process.env.MONGODB_URL}`).then((res) =>{
+    console.log("DB connected Successfully")
+  } );
+  
+} catch (error) {
+  console.log(error)
+}
+
 const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.send('GET request to the homepage')
+})
 
 app.post("/register", (req, res) => {
   console.log(req)
