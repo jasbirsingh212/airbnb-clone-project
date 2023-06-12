c6318dd5446b97bf9642c8bb71de0d21c4a9ba0a
Jasbir Singh <jasbir212singh@gmail.com>
2023-06-12 18:18:40 +0530

    completed login and session token auth

--- client/src/utills/context/index.jsx
+++ client/src/utills/context/index.jsx
@@ -1,4 +1,5 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
+import customAxios from "./../axios/index";
 
 export const UserContext = createContext({
 
@@ -7,6 +8,20 @@ export const UserContext = createContext({
 
 const ContextProvider = ({children}) => {
   const [user, setUser] = useState();
+
+  useEffect(() => {
+    
+    const ApiCall = async () => {
+      const {data} = await customAxios.get('/profile');
+      setUser(data)
+    }
+
+
+    if(!user)
+    ApiCall();
+    
+  }, [])
+  
 
   return (
     <UserContext.Provider value={{
